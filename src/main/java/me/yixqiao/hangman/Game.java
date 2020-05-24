ee30f661e8fac103aa8f88d9a5a68afd38382abf
Yixuan Qiao <yixqiao@gmail.com>
2020-05-24 16:55:51 -0700

    Add reading words from file

--- src/main/java/me/yixqiao/hangman/Game.java
+++ src/main/java/me/yixqiao/hangman/Game.java
@@ -1,4 +1,37 @@
 package me.yixqiao.hangman;
 
 public class Game {
+    public StringBuilder word;
+    public String actualWord;
+    public int wrong = 0;
+
+    public Game(int wLen) {
+        word = new StringBuilder();
+        for (int i = 0; i < wLen; i++)
+            word.append(".");
+
+    }
+
+    public Game(String actualWord) {
+        word = new StringBuilder();
+        this.actualWord = actualWord.toLowerCase();
+        for (int i = 0; i < actualWord.length(); i++)
+            word.append(".");
+    }
+
+    public int guess(char c) {
+        assert actualWord.length() == word.length();
+        int changed = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (actualWord.charAt(i) == c) {
+                word.setCharAt(i, c);
+                changed++;
+            }
+        }
+        if (changed == 0)
+            wrong++;
+        return changed;
+    }
+
+
 }
--- src/main/java/me/yixqiao/hangman/Main.java
+++ src/main/java/me/yixqiao/hangman/Main.java
@@ -1,4 +1,49 @@
 package me.yixqiao.hangman;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
 public class Main {
+    static ArrayList<String> words;
+
+    public static void main(String[] args) {
+        loadWords();
+        guessWord("pineapple");
+    }
+
+    private static void guessWord(String correctWord) {
+        Game game = new Game(correctWord);
+        ArrayList<String> candidates = new ArrayList<>();
+        String word = game.word.toString();
+        int[] freq = new int[26];
+
+        for (String curW : words) {
+            if (curW.length() == correctWord.length() && Pattern.matches(word, curW)) {
+                candidates.add(curW);
+                for(int c=0; c<curW.length(); c++){
+                    freq[curW.charAt(c) - 97]++;
+                }
+            }
+        }
+
+        // for(int i=0; i<26; i++){
+        //     System.out.printf("%s: %d\n", (char)(i+97), freq[i]);
+        // }
+    }
+
+    private static void loadWords() {
+        words = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File("words/words.txt")));
+            String line;
+            while ((line = br.readLine()) != null && line.length() != 0) {
+                words.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("Read %d words.", words.size());
+    }
+
 }
